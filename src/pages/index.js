d28137a1435fbde261f7dab7ea92cfb025d07eee
Anh Nguyen <anguyen1@mscs256-28382.olinriclab.macalester.edu>
2019-12-08 16:16:33 -0600

    Added design outline

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Connexio" />
     <h1 class="tc">Overview</h1>
-    <p class="tc">Connexio provides a platform for cultural learning on Macalester campus.</p>
+    <p class="tc">Connexio provides a platform for <b>cultural learning</b> on Macalester campus.</p>
       <div class="flex justify-around">
         <div class="tc pa3">
-        <code>SHARE</code>
+        <code class="b">SHARE</code>
         <p>Post about your own cultural experiences using pictures, videos, or text.
         Share with other users in real-time, and include your location if you
         want your post to show up on our map.</p>
         </div>
         <div class="tc pa3">
-        <code>ENGAGE</code>
+        <code class="b">ENGAGE</code>
         <p>We've made it easier for you to find information about campus cultural events
         by creating one central calendar. Post about events happening
         in the Macalester community, and RSVP to ones
@@ -70,16 +70,23 @@ const IndexPage = () => (
     <img src={require(`../images/Task2-Storyboard.jpg`)}/>
     <h1 class="tc">Video Storyboard</h1>
     <h1 class="tc">Design Process</h1>
-
+    <div class="tc">
+    <code class="b">First Paper Prototype</code>
+    </div>
+    <div class="tc">
+    <code class="b">Second Paper Prototype</code>
+    </div>
+    <div class="tc">
+    <code class="b">Digital Mockup</code>
+    </div>
     <h1 class="tc">Reports and Presentation</h1>
     <h1 class="tc">Team Members</h1>
     <div class="flex justify-around">
-    <p class="tc">Anh Nguyen</p>
-    <p class="tc">Katia Sievert</p>
-    <p class="tc">Yiping Zhong</p>
-    <p class="tc">Zain Chaudhry</p>
+    <a href="https://github.com/theang66" style={{ "text-decoration": "none", "color": "blue" }}>Anh Nguyen</a>
+    <a href="https://github.com/ksievert" style={{ "text-decoration": "none", "color": "blue" }}>Katia Sievert</a>
+    <a href="https://github.com/YipingZhong" style={{ "text-decoration": "none", "color": "blue" }}>Yiping Zhong</a>
+    <a href="https://github.com/ZainMahmoodC" style={{ "text-decoration": "none", "color": "blue" }}>Zain Chaudhry</a>
     </div>
-
   </Layout>
 )
 
