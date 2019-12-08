292d9539c6e99d574602ddadfe22005d91a5615a
Anh Nguyen <anguyen1@mscs256-28382.olinriclab.macalester.edu>
2019-12-08 15:18:16 -0600

    Added some stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import "tachyons"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,14 +9,27 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Connexio" />
-    <h1>Overview</h1>
-    <h1>Video Storyboard</h1>
-    <h1>Design Process</h1>
-    <h1>Task 1</h1>
-    <h1>Task 2</h1>
-    <h1>Reports</h1>
-    <h1>Presentation</h1>
-    <h1>Team Members</h1>
+    <h1 class="tc">Overview</h1>
+    <p class="tc">Connexio provides a platform for cultural learning on Macalester campus.</p>
+      <div class="flex justify-around">
+        <div class="tc">
+        <code>SHARE</code>
+        </div>
+        <div class="tc">
+        <code>ENGAGE</code>
+        </div>
+      </div>
+    <h1 class="tc">Task 1</h1>
+    <h1 class="tc">Task 2</h1>
+    <h1 class="tc">Video Storyboard</h1>
+    <h1 class="tc">Design Process</h1>
+
+    <h1 class="tc">Reports and Presentation</h1>
+    <h1 class="tc">Team Members</h1>
+    <p class="tc">Anh Nguyen</p>
+    <p class="tc">Katia Sievert</p>
+    <p class="tc">Yiping Zhong</p>
+    <p class="tc">Zain Chaudhry</p>
   </Layout>
 )
 
